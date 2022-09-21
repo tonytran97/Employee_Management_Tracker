a520e082caec96d9bfefb93f3329b966fcb86e0d
tonytran97 <tonytran_97@live.com>
2022-09-20 21:32:14 -0700

    built seeds foundation, updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    department_name VARCHAR(30) NOT NULL, 
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL, 
 );
 
 CREATE TABLE role (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL(18,2) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE SET NULL
-    
 );
 
 CREATE TABLE employee (
-    id INT, 
+    id INT AUTO_INCREMENT, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL, 
     role_id INT, 
-    manager_id INT,
-    FOREIGN KEY (employee_id) REFERENCES role(id)
+    manager_id INT AUTO_INCREMENT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
     ON DELETE SET NULL
 )
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+INSERT INTO role (title, salary)
+VALUES ('Sales Lead', 100000),
+       ('Salesperson', 80000),
+       ('Lead Engineer', 150000),
+       ('Software Engineer', 120000),
+       ('Account Manager', 160000),
+       ('Accountant', 125000),
+       ('Legal Team Lead', 250000),
+       ('Lawyer', 190000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ('Joseph', 'Wong'),
+       ('Amanda', 'May'),
+       ('Brent', 'Lawrence');
